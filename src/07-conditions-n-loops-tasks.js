1ea3546faf615668aaaf469b27679ac37f05d360
Elena Pavlova <lenlho22@gmail.com>
2022-01-13 00:21:07 +0300

    feat: implement reverseString, reverseInteger and getDigitalRoot

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -234,8 +234,8 @@ function getIntervalString(/* a, b, isStartIncluded, isEndIncluded */) {
  * 'rotator' => 'rotator'
  * 'noon' => 'noon'
  */
-function reverseString(/* str */) {
-  throw new Error('Not implemented');
+function reverseString(str) {
+  return str.split(' ').reverse().map((word) => word.split('').reverse().join('')).join(' ');
 }
 
 
@@ -251,8 +251,10 @@ function reverseString(/* str */) {
  *   87354 => 45378
  *   34143 => 34143
  */
-function reverseInteger(/* num */) {
-  throw new Error('Not implemented');
+function reverseInteger(num) {
+  const strNum = String(num).split('').reverse().join('');
+
+  return Number(strNum);
 }
 
 
@@ -294,8 +296,14 @@ function isCreditCardNumber(/* ccn */) {
  *   10000 ( 1+0+0+0+0 = 1 ) => 1
  *   165536 (1+6+5+5+3+6 = 26,  2+6 = 8) => 8
  */
-function getDigitalRoot(/* num */) {
-  throw new Error('Not implemented');
+function getDigitalRoot(num) {
+  if (num < 10) {
+    return num;
+  }
+
+  const iterSum = String(num).split('').reduce((acc, item) => acc + Number(item), 0);
+
+  return getDigitalRoot(iterSum);
 }
 
 
